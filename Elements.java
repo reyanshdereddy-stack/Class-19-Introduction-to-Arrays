d8be5ad0bfbdd892fb8391da22fac165bf975710
reyanshdereddy-stack <reyanshdereddy@gmail.com>
2026-03-16 19:52:13 -0400

    v2

--- Elements.java
+++ Elements.java
@@ -1,7 +1,28 @@
 import java.util.Scanner;
-class Elements {
+
+public class Elements {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("");
+    Scanner scn = new Scanner(System.in);
+    System.out.print("Enter the element to search for: ");
+    int d = scn.nextInt();
+    System.out.print("Enter the number of elements in the array: ");
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    System.out.println("Enter the " + n + " elements of the array:");
+    for(int i = 0; i < n; i++) {
+    arr[i] = scn.nextInt(); // Store each input value in the array
+    }
+
+    int ans = find(arr, d);
+    System.out.println(ans);
+    }
+    public static int find(int[] arr, int d) {
+    for(int i = 0; i < arr.length; i++) {
+    if(arr[i] == d) { // If the element is found
+    return i; // Return the index of the element
+    }
+    }
+    // If the element is not found, return -1
+    return -1;
     }
 }
